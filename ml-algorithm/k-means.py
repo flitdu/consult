6c552f8b46b2c8f50cf853f520c51e6020709ea4
dufy <813540660@qq.com>
2021-06-25 08:18:23 +0800

    使用 numpy

--- ml-algorithm/k-means.py
+++ ml-algorithm/k-means.py
@@ -11,17 +11,29 @@ Description :
 3）k 值如何定？？
 
 """
+import pandas as pd
+import numpy as np
 
 data_watermelon = "../data/西瓜数据集4.0.txt"
-
+def load_data(path):
+    df = pd.read_csv(data_watermelon, sep='\t')
+    return df.loc[:,['density','sugar']].values  # 转numpy
 
 class KMeans:
-    pass
+    def __init__(self, path, k=2):
+        self.k = k
+        self.data = load_data(path)
+        self.centroid = [[0,0] for _ in range(k)]
+
+    # def update_cen
 
 
 
 if __name__ == "__main__":
     pass
-    f = open(data_watermelon, 'r')
-    for line in f:
-        print(line.strip())
+
+
+    k_means = KMeans(data_watermelon, 4)
+    print(k_means.k)
+    print(k_means.centroid)
+    print(k_means.data)
