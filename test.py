037bf1d6ec713af4363e011ea2d626e2480ec291
dufy <813540660@qq.com>
2021-07-25 00:06:44 +0800

    tf学习3

--- test.py
+++ test.py
@@ -1,13 +1,37 @@
+import tensorflow as tf
 
+# 代码示例：
 
+class CNN(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=32,             # 卷积层神经元（卷积核）数目
+            kernel_size=[5, 5],     # 感受野大小
+            padding='same',         # padding策略（vaild 或 same）
+            activation=tf.nn.relu   # 激活函数
+        )
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=64,
+            kernel_size=[5, 5],
+            padding='same',
+            activation=tf.nn.relu
+        )
+        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
+        self.flatten = tf.keras.layers.Reshape(target_shape=(7 * 7 * 64,))
+        self.dense1 = tf.keras.layers.Dense(units=1024, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=10)
 
-class Student(object):
-    def __init__(self, name):
-        self.name =name
-    def __str__(self):
-        return f'Student object {self.name}'
+    def call(self, inputs):
+        x = self.conv1(inputs)                  # [batch_size, 28, 28, 32]
+        x = self.pool1(x)                       # [batch_size, 14, 14, 32]
+        x = self.conv2(x)                       # [batch_size, 14, 14, 64]
+        x = self.pool2(x)                       # [batch_size, 7, 7, 64]
+        x = self.flatten(x)                     # [batch_size, 7 * 7 * 64]
+        x = self.dense1(x)                      # [batch_size, 1024]
+        x = self.dense2(x)                      # [batch_size, 10]
+        output = tf.nn.softmax(x)
+        return output
 
-
-print(Student('cc'))
-s= Student('HH')
-print(s)
+    model = tf.keras.Sequential()
