4d7910baf77d7ef885925569f3857d8bbf7501cf
dufy <813540660@qq.com>
2021-08-11 00:06:06 +0800

    tf keras 4

--- test.py
+++ test.py
@@ -28,6 +28,25 @@ input
 """
 
 
-class MyDense(layers.Layer): # 自定义网络层
-    def __init__(self, inp_dim, outp_dim):
-        super(MyDense, self).__init__()
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential, losses, optimizers, datasets
+
+# 创建4层全连接
+model = keras.Sequential([
+    layers.Dense(256, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(10, activation='relu'),
+
+])
+model.build(input_shape=(4,784))
+model.summary()
+
+
+
+
+input_shape = (1, 28, 28, 3)
+x = tf.random.normal(input_shape)
+y = tf.keras.layers.Conv2D(
+    1, 3, input_shape=input_shape)(x)  # filters： 2， kernel_size: 3
