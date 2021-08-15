b756bb88a035e22e7445ca01c0ab4e831a7549da
dufy <813540660@qq.com>
2021-08-15 12:12:40 +0800

    tf2

---
+++ jupyter/tensorflow2/README.md
@@ -0,0 +1,7 @@
+关于TF2的学习
+
+参考：
+
+- 龙书
+- [lyhue1991](https://github.com/lyhue1991)/**[eat_tensorflow2_in_30_days](https://github.com/lyhue1991/eat_tensorflow2_in_30_days)**
+
--- test.py
+++ test.py
@@ -1,52 +1,12 @@
+# 二分类问题选择二元交叉熵损失函数
+model.compile(optimizer='adam',
+            loss='binary_crossentropy',
+            metrics=['AUC'])
 
+history = model.fit(x_train,y_train,
+                    batch_size= 64,
+                    epochs= 30,
+                    validation_split=0.2 #分割一部分训练数据用于验证
+                   )
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import Sequential
-
-layers_num=2  # 堆叠两次
-network = Sequential([])
-for _ in range(layers_num):
-    network.add(layers.Dense(3))
-    network.add(layers.ReLU())
-network.build(input_shape=(None, 4))
-print(network.summary())
-
-"""
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
- ⭕️     |       |       |
-input   
-
-
-"""
-
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers, Sequential, losses, optimizers, datasets
-
-# 创建4层全连接
-model = keras.Sequential([
-    layers.Dense(256, activation='relu'),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(10, activation='relu'),
-
-])
-model.build(input_shape=(4,784))
-model.summary()
-
-
-
-
-input_shape = (1, 28, 28, 3)
-x = tf.random.normal(input_shape)
-y = tf.keras.layers.Conv2D(
-    1, 3, input_shape=input_shape)(x)  # filters： 2， kernel_size: 3
+tf.keras.backend.clear_session()
