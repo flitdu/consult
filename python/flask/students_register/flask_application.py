6e1ba45f5e2f42b525ec76ea5f96682e558eb197
dufy <813540660@qq.com>
2022-01-20 22:09:41 +0800

    模板使用

--- python/flask/students_register/flask_application.py
+++ python/flask/students_register/flask_application.py
@@ -14,19 +14,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route("/")
 def index():
     name = request.args.get("name", "world")  # 默认值 world
     return render_template("index.html", PARA=name)
     # return "hello, world"
 
+
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")  # form 对应 post; args对应 get
     dorm = request.form.get("dorm")
     if not name or not dorm:
-        return "failure"
-    return render_template("success.html")
+        # return "failure"
+        return render_template("failure.html")
+    else:
+        return render_template("success.html")
 
 
 if __name__ == "__main__":
