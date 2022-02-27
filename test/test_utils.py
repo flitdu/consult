9777a8e77779237634cdff3ec0fa581936de7bc0
dufy <813540660@qq.com>
2022-02-27 21:21:14 +0800

    文件变动

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,15 +1,3 @@
-from sys import argv
-from PIL import Image
-
-if len(argv) !=4:
-    exit("usage: python resize.py n file outfile")
-
-n = int(argv[1])
-infile = argv[2]
-outfile = argv[3]
-
-inimage = Image.open(infile)
-width, height = inimage.size
-outimage = inimage.resize((width * n, height * n))
-
-outimage.save(outfile)
+print('ssdg')
+a= {'s':1}
+print(a['s'])
